e639677db8f63b06b303db05d03eb200c3829475
schneefux <schneefux+commit@schneefux.xyz>
2020-05-09 10:16:42 +0200

    Style scrollbar

--- src/renderer/layouts/default.vue
+++ src/renderer/layouts/default.vue
@@ -1,3 +1,18 @@
 <template>
   <nuxt class="bg-background-700 text-white"></nuxt>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: 8px; /* vertical scrollbars */
+  height: 8px; /* horizontal scrollbars */
+}
+
+::-webkit-scrollbar-track {
+  background: rgba(255, 255, 255, 0.1);
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgba(255, 255, 255, 0.7);
+}
+</style>
