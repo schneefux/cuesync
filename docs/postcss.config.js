4a3c06ca0da918c034cf4898c54505d523690fd3
schneefux <schneefux+commit@schneefux.xyz>
2020-06-08 08:58:03 +0200

    Split docs into seperate package

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./docs/**/*.html'],
+  content: ['./**/*.html'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
 
