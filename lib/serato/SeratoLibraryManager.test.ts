45af0a90f13bf50e951580d42e26f11c1520bdf5
schneefux <schneefux+commit@schneefux.xyz>
2020-06-10 11:32:53 +0200

    Remove FLACs and LFS

--- lib/serato/SeratoLibraryManager.test.ts
+++ lib/serato/SeratoLibraryManager.test.ts
@@ -24,21 +24,6 @@ test('should read track info from mp3', async () => {
   })
 })
 
-test('should read track info from flac', async () => {
-  const trackInfo = await library.loadTrack(path.join(fixtures, 'retro_funky.flac'))
-
-  expect(trackInfo).toMatchObject({
-    "album": "Retro Funky",
-    "artists": ["Perséphone"],
-    "bpm": 115,
-    "genre": undefined,
-    "isrc": "NLPM11407110",
-    "key": "Em",
-    "title": "Retro Funky (SUNDANCE Remix)",
-    "durationSeconds": 213,
-  })
-})
-
 test('should write track info to mp3', async () => {
   await copyFile(path.join(fixtures, 'retro_funky.mp3'), path.join(fixtures, 'tmp', 'retro_funky.mp3'))
 
@@ -48,20 +33,6 @@ test('should write track info to mp3', async () => {
   })
 
   const tags = await taglib.readTags(path.join(fixtures, 'tmp', 'retro_funky.mp3'))
-  expect(tags).toMatchObject({
-    TITLE: ['Testing 123'],
-  })
-})
-
-test('should write track info to flac', async () => {
-  await copyFile(path.join(fixtures, 'retro_funky.flac'), path.join(fixtures, 'tmp', 'retro_funky.flac'))
-
-  await library.update({
-    path: path.join(fixtures, 'tmp', 'retro_funky.flac'),
-    title: 'Testing 123',
-  })
-
-  const tags = await taglib.readTags(path.join(fixtures, 'tmp', 'retro_funky.flac'))
   expect(tags).toMatchObject({
     TITLE: ['Testing 123'],
   })
